6e388708af21748b8e75d380447f7343c724f91c
Razvan Andrei <chiper_raizy@yahoo.com>
2025-03-04 06:19:50 +0100

    adding create a post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express"; // import express
-import { getAllPosts } from "../controllers/postsController.js"; // import controllers
+import { getAllPosts,createPost, } from "../controllers/postsController.js"; // import controllers
 
 const router = express.Router(); // create a router
 
 router.get("/posts", getAllPosts); // get all posts /api/posts
+router.post("/posts", createPost); // create a post /api/posts
 // export router
 export default router;
