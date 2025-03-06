b0f5ff585b4ac3f878cd1d1c6e847331ef76e154
Maksim Shupta <maxshupta@yahoo.com>
2025-03-06 13:07:22 +0100

    errors fixes usersController: isMatch deactivated

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,25 +1,23 @@
 import { db } from "../services/db.js";
 import bcrypt from "bcryptjs";
 
-//const app = express();
-//app.use(express.json());
-
 export const getUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
-    ]);
+    const result = await db.query(
+      "SELECT * FROM users WHERE email = $1 password=$2",
+      [email, password]
+    );
 
     if (result.rows.length === 0) {
       return res.status(404).send("User not found");
     }
     const user = result.rows[0];
     // Compare hashed password
-    const isMatch = await bcrypt.compare(password, user.password);
+    /*const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).send("Invalid credentials");
-    }
+    }*/
     res.setHeader("Content-Type", "application/json");
     res.json({ id: user.id, email: user.email }); //???
   } catch (error) {
@@ -27,9 +25,3 @@ export const getUser = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
-//app.post("/api/users", getUser);
-
-// Start server
-/*app.listen(5001, () => {
-  console.log("Server running on port 5001");
-});*/
