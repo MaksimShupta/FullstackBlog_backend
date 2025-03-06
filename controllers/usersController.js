0d60347a4458caf072028c686feb24e9804d5fc2
Maksim Shupta <maxshupta@yahoo.com>
2025-03-06 12:45:44 +0100

    errors fixes2

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,10 @@
 import { db } from "../services/db.js";
 import bcrypt from "bcryptjs";
 
-const app = express();
-app.use(express.json());
+//const app = express();
+//app.use(express.json());
 
-const getUser = async (req, res) => {
+export const getUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
@@ -27,9 +27,9 @@ const getUser = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
-app.post("/api/users", getUser);
+//app.post("/api/users", getUser);
 
 // Start server
-app.listen(5001, () => {
+/*app.listen(5001, () => {
   console.log("Server running on port 5001");
-});
+});*/
